177ce20980091276f41e4446fb088d45716576f7
Iamneo <techadmin@iamneo.ai>
2023-08-27 07:53:26 +0000

    Backup Commit

---
+++ reactapp/src/App.js
@@ -0,0 +1,20 @@
+import './index.css';
+import Search from './components/Search/Search';
+
+function App() {
+  return (
+    <div className="App">
+      <Search />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+    </div>
+  );
+}
+
+export default App;
+
